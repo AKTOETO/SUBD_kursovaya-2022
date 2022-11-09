d4e8ae3cec98b6d9855f9444bf97488151043867
Bogdan <bogdan.plockiy@mail.ru>
2022-11-10 01:44:38 +0300

    added my_list in main.cpp

--- kursovaya-2022/kursovaya-2022.cpp
+++ kursovaya-2022/kursovaya-2022.cpp
@@ -1,8 +1,8 @@
-﻿
-
-#include <iostream>
+﻿#include <iostream>
+//#include "../LW4-2022/LW4/LW4.hpp"
+#include"../LW4/my_list/my_list.hpp"
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    
 }
