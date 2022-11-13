68b0b95c489ed81934efb1e2756ba72e357b210b
Bogdan <bogdan.plockiy@mail.ru>
2022-11-13 21:00:05 +0300

    read from file & input_and_check

--- kursovaya-2022/kursovaya-2022.cpp
+++ kursovaya-2022/kursovaya-2022.cpp
@@ -9,6 +9,9 @@
 // заполнение len элементов элементом symb
 #define OUT_W(symb, len) fixed << setfill(symb) << setw(len)
 
+// путь до файла с базой данных
+const string db_file_path = "../database/db.txt";
+
 // шапка таблицы
 const string table_cap = "| НОМЕР | НОСИТЕЛЬ | НАЗВАНИЕ |\
  ИМЯ/ФАМИЛИЯ ИСПОЛНИТЕЛЯ | ВРЕМЯ | КОЛ-ВО ВОСПР. | ЦЕНА |\n";
@@ -18,6 +21,61 @@ const int width_of_fields[8] =
 {
 	7,10,10,12,13,7,15,6
 };
+
+// строка с коммандами
+const char* command_str =
+"\nВведите номер комманды:\n\
+\t1. Выход.\n\
+\t2. Считать базу данных из файла.\n\
+\t3. Считать музыкальный товар из консоли.\n\
+\t4. Сортировать....\n\
+\t5. \n\
+\t6. \n\
+\t7. \n\
+\t8. \n\
+\t9. \n\
+\t10.";
+
+// коды для взаимодействия пользователья с программой
+enum class input_codes
+{
+	exit = 1,
+	file_read,
+	console_read,
+	sort
+};
+
+// ввод и проверка значений
+template<typename T>
+T input_and_check(T _min, T _max,
+	const char* welcome_str,
+	const char* err_str = "Было введено некорректное значение")
+{
+	// размер массива
+	T num;
+
+	// вывод сообщения
+	cout << welcome_str << "\n";
+	cin >> num;
+
+	// если было введено некорректное значение
+	if (num > _max || num < _min) {
+		// если была введена не цифра
+		if (cin.fail())
+		{
+			cin.clear();
+			cin.ignore(INT_MAX, '\n');
+		}
+
+		// отчистка консоли
+		//system("cls");
+		cout << err_str << "\n";
+
+		// рекурсивное обращение
+		num = input_and_check(_min, _max, welcome_str, err_str);
+	}
+	return num;
+}
 
 // получение подстроки отделенной с помощью delim
 // и удаление этой подстроки из изначальной строки
@@ -148,18 +206,69 @@ struct MusicStuff
 	}
 };
 
-
-enum class input_codes
+// считывание списка из файла
+template<class T>
+void read_from_file(my_list<T>& _list)
 {
-	file_read = 0,
-	console_read,
-	sort
-};
+	// открытие файла
+	ifstream fin(db_file_path);
 
-// TODO: функция взаимодействия с пользователем
+	// если файл не был открыт
+	if (!fin.is_open())
+	{
+		cout << "файл не был открыт\n";
+		return;
+	}
+
+	// считывание данных
+	while (fin.peek() != EOF)
+	{
+		_list.push(fin);
+	}
+}
+
+// функция взаимодействия с пользователем
 void dialog()
 {
+	// переменная содержащая коды действий
+	input_codes in_code;
 
+	// список с музыкальными товарами
+	my_list<MusicStuff> music_list;
+
+	do
+	{
+		// запрос у пользователя следующих действий
+		in_code = input_codes(input_and_check(1, 10, command_str));
+
+		// запуск соответствующих функций
+		switch (in_code)
+		{
+		case input_codes::exit:
+			INFO("Выход из программы");
+			exit(0);
+			break;
+
+		case input_codes::file_read:
+			INFO("Чтение из файла " + db_file_path);
+			read_from_file(music_list);
+			break;
+
+		case input_codes::console_read:
+			INFO("Чтение из консоли");
+			// <ПОР.НОМЕР>:<НОСИТЕЛЬ>:<НАЗВАНИЕ>:<ИМЯ ИСПОЛН.>:<ФАМИЛ. ИСПОЛН.>:<ВРЕМЯ>:<ВОСПРОИЗВ.>:<ЦЕНА>
+
+			break;
+
+		case input_codes::sort:
+			break;
+
+		default:
+			cout << "Неверный код\n";
+			break;
+		}
+
+	} while (in_code != input_codes::exit);
 }
 
 int main()
